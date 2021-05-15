395119fc06ea9fbc3874774924ab3ce7a497db7b
katzper <kacper.wozni@gmail.com>
2021-05-15 12:49:07 +0200

    Correct format of saving to json file

---
+++ crawler/scraper.py
@@ -0,0 +1,116 @@
+# %%
+import os
+import time
+import json
+import scrapy
+import logging
+import datetime
+import pandas as pd
+import scrapy.crawler as crawler
+from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
+
+
+class OtoMotoScraper(scrapy.Spider):
+    name = "otomotospider"
+
+    custom_settings = {
+        "DOWNLOAD_DELAY": "1.0",
+        "AUTOTHROTTLE_ENABLED": True,
+        "DOWNLOADER_MIDDLEWARES": {
+            "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+            "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
+        }
+    }
+
+    def start_requests(self):
+        urls = [
+            "https://www.otomoto.pl/osobowe/",
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        self.logger.info(
+            "Got successful response from {}".format(response.url))
+
+        soup = BeautifulSoup(response.body, "lxml")
+
+        offers = soup.find("div", attrs={"class": "offers list"})
+
+        offer_links = [link.get("href") for link in offers.find_all(
+            "a", attrs={"class": "offer-title__link"})]
+
+        for item_url in offer_links:
+            if "carsmile" in item_url:
+                continue
+            else:
+                yield scrapy.Request(url=item_url, callback=self.parse_offer_details)
+
+        next_page = response.xpath(
+            '//*[@id="body-container"]/div[2]/div[2]/ul/li[7]/a/@href').get()
+        if next_page is not None:
+            yield scrapy.Request(url=next_page, callback=self.parse)
+
+    def parse_offer_details(self, response):
+        self.logger.info(
+            "Got successfull response from {html}".format(html=response.url))
+
+        details_dict = {}
+
+        soup = BeautifulSoup(response.body, "lxml")
+
+        details = soup.find_all("li", attrs={"class": "offer-params__item"})
+
+        for index, detail in enumerate(details):
+            if detail.find("a", attrs={"offer-params__link"}):
+                key = detail.find("span", attrs={"offer-params__label"}).text
+                value = detail.find("a", attrs={"offer-params__link"}).text
+
+                if isinstance(key, str) & isinstance(value, str):
+                    details_dict[key.strip()] = value.strip()
+
+            else:
+                key = detail.find("span", attrs={"offer-params__label"}).text
+                value = detail.find("div", attrs={"offer-params__value"}).text
+
+                if isinstance(key, str) & isinstance(value, str):
+                    details_dict[key.strip()] = value.strip()
+
+        details_dict["cena"] = soup.find(
+            "span", attrs={"class": "offer-price__number"}).text.strip()
+        details_dict["lokalizacja"] = soup.find(
+            "span", attrs={"class": "seller-box__seller-address__label"}).text.strip()
+
+        features = soup.find_all("li", attrs={"class": "offer-features__item"})
+
+        details_dict["wyposazenie"] = [feature.text.strip()
+                                       for feature in features]
+
+        self.save_to_file(details_dict)
+
+    def save_to_file(self, to_save):
+
+        save_path = "C:/Users/kacpe/OneDrive/Dokumenty/Programming/web-scraper/results/otomoto_passanger_car_data_"
+        file_path = save_path + \
+            str(datetime.date.today().strftime("%d_%m_%Y")) + ".json"
+
+        if os.path.isfile(file_path):
+            with open(file_path, mode="a") as out_file:
+                json.dump(to_save, out_file)
+                out_file.write("\n")
+
+        else:
+            with open(file_path, mode="w") as out_file:
+                json.dump(to_save, out_file)
+                out_file.write("\n")
+
+
+# %%
+if __name__ == "__main__":
+    process = CrawlerProcess({"LOG_LEVEL": "INFO"})
+    process.crawl(OtoMotoScraper)
+    spider = next(iter(process.crawlers)).spider
+    process.start()
+
+# %%
